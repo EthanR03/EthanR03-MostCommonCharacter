f55c5116541787f0533d20945816c7da206b9bfb
EthanR03 <EthanR03@github.com>
2025-10-28 16:32:33 +0000

    Finished!

--- src/main/java/MostCommonCharacter.java
+++ src/main/java/MostCommonCharacter.java
@@ -8,6 +8,25 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        if (str == null || str.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        int[] charCounts = new int[256];
+
+        for (char c : str.toCharArray()) {
+            charCounts[c]++;
+        }
+
+        int maxCount = -1;
+        char mostCommonChar = ' ';
+
+        for (int i = 0; i < charCounts.length; i++) {
+            if (charCounts[i] > maxCount) {
+                maxCount = charCounts[i];
+                mostCommonChar = (char) i;
+            }
+        }
+        return mostCommonChar;
     }
 }
